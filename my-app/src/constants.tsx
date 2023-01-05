d43a8f42208fbf68ec7fe163321fe936b90b30e1
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2023-01-05 23:06:46 +0700

    Move color constants and add API URL

---
+++ my-app/src/constants.tsx
@@ -0,0 +1,9 @@
+export const Colors = {
+    blue: "#1F2E6E",
+    red: "#FF3A3A",
+    dark_grey: "#2D2D2F",
+    white: "#F5F5F5",
+    yellow: "#EFB61E",
+}
+
+export const API_URL = "http://localhost:8080";
