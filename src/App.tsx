473bc3cf53f2af279d3117147ba2bf93f040267b
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2023-01-06 02:14:22 +0700

    Add background component

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,42 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import styled from 'styled-components';
 import './App.css';
+import ModulePageThread from './components/threads/ModulePageThread';
+import { Colors } from './constants';
+
+const Background = styled.div`
+    background-color: ${Colors.blue};
+    width: 100%;
+    background-size: cover;
+    min-height: 100vh;
+`
 // import ModulePageThread from './components/threads/ModulePageThread';
 
-function TestCORS() {
-  const makeAPICall = async () => {
-    try {
-      const response = await fetch('http://localhost:8080/ping', {mode:'cors'});
-      const data = await response.json();
-      console.log({ data })
-    }
-    catch (e) {
-      console.log(e)
-    }
-  }
-  useEffect(() => {
-    makeAPICall();
-  }, [])
-  return (
-    <div className="App">
-      <h1>React Cors Guide</h1>
-    </div>
-  );
-}
+// function TestCORS() {
+//   const makeAPICall = async () => {
+//     try {
+//       const response = await fetch('http://localhost:8080/ping', {mode:'cors'});
+//       const data = await response.json();
+//       console.log({ data })
+//     }
+//     catch (e) {
+//       console.log(e)
+//     }
+//   }
+//   useEffect(() => {
+//     makeAPICall();
+//   }, [])
+//   return (
+//     <div className="App">
+//       <h1>React Cors Guide</h1>
+//     </div>
+//   );
+// }
 
 function App() {
   return (
-    <div className="App">
-      <TestCORS/>
-    </div>
+    <Background>
+      <ModulePageThread threadId={2} />
+    </Background>
   );
 }
 
