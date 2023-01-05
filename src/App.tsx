3701b038c6c92473f283ff45a35240975b0233a4
albertarielw <albertariel2002@gmail.com>
2023-01-06 01:47:13 +0800

    test cors

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
-import ModulePageThread from './components/threads/ModulePageThread';
+// import ModulePageThread from './components/threads/ModulePageThread';
+
+function TestCORS() {
+  const makeAPICall = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/ping', {mode:'cors'});
+      const data = await response.json();
+      console.log({ data })
+    }
+    catch (e) {
+      console.log(e)
+    }
+  }
+  useEffect(() => {
+    makeAPICall();
+  }, [])
+  return (
+    <div className="App">
+      <h1>React Cors Guide</h1>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <ModulePageThread/>
+      <TestCORS/>
     </div>
   );
 }
