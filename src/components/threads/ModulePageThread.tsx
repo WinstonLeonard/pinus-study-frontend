217411364aef93cc141f2dc5f2e20a9b20b51fc1
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2023-01-06 02:15:37 +0700

    Make module page thread dynamic

--- src/components/threads/ModulePageThread.tsx
+++ src/components/threads/ModulePageThread.tsx
@@ -4,10 +4,11 @@ import { API_URL, Colors } from '../../constants';
 import { Thread, ThreadInitialState } from '../../features/threads/threadSlice';
 import CommentOutlinedIcon from '@mui/icons-material/CommentOutlined';
 
-const ThreadContainer = styled.div`
+const ThreadContainer = styled.button`
     background-color: ${Colors.white};
     width: 50vw;
     border-radius: 20px;
+    border: none;
     padding: 1.5em;
     text-align: left;
 
@@ -21,45 +22,68 @@ const StyledQuestionTitle = styled.span`
     font-size: 1.875em; 
 `
 
-const PostedBy = styled.span`
+const RegularText = styled.span`
     font-family: 'Poppins', sans-serif;
     font-size: 1.25em; 
 `
-const PostedSince = styled(PostedBy)`
+const PostedSince = styled(RegularText)`
     float: right;
 `
 const TextArea = styled.span`
+    display: inline-block;
     font-family: 'Poppins-Medium', sans-serif;
     font-size: 1.5em; 
+    margin-top: 0.5em;
+    margin-bottom: 0.5em;
 `
 
-const ModulePageThread = () => {
+/**
+ * Thread preview shown on the Module Page.
+ * 
+ * @param threadId The thread id to fetch the data.
+ */
+const ModulePageThread = ({threadId} : any) => {
     const [thread, setThread] = useState<Thread>(ThreadInitialState);
 
-    // Hook to fetch data
+    /**
+     * Hook to fetch thread data.
+     */
     useEffect(() => {
-        fetch(API_URL + "/thread/2")
+        fetch(API_URL + `/thread/${threadId}`)
             .then((response) => response.json())
             .then((data) => {
-                setThread(data);
-                console.log(data);
+                setThread(data.thread);
             })
             .catch((error) => {
                 console.log(error);
             })
     }, [])
 
+    /**
+     * Shortens the content to max. 150 characters to prevent the 
+     * post preview from being too long.
+     * 
+     * @param content The full content of the thread.
+     * @returns The shortened content of the thread.
+     */
+    const shortenLongPosts = (content : string) : string => {
+        if (content.length > 150) {
+            return content.substring(0, 150) + "...";
+        }
+        return content;
+    }
+
     return (
         <ThreadContainer>
             <PostedSince>4d</PostedSince>
-            <StyledQuestionTitle>Question Title</StyledQuestionTitle>
+            <StyledQuestionTitle>{thread.Title}</StyledQuestionTitle>
             <br/>
-            <PostedBy>Posted by @why_am_i_still_here</PostedBy>
+            <RegularText>Posted by {thread.Username}</RegularText>
             <br/>
-            <TextArea>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum bibendum sem eget neque maximus, sit amet placerat lacus ultrices. Mauris eget maximus turpis</TextArea>
+            <TextArea>{shortenLongPosts(thread.Content)}</TextArea>
             <br/>
-            <CommentOutlinedIcon sx={{fontSize: "1.25em"}}/>
-            <PostedBy> 37</PostedBy>
+            <CommentOutlinedIcon sx={{fontSize: "1.375em"}}/>
+            <RegularText> {thread.Comments || 0}</RegularText>
         </ThreadContainer>
     )
 }
