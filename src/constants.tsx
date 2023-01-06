dd1cb0785af83ba9cf74ab662a9543ce5fa8ef73
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2023-01-06 23:57:18 +0700

    Add light_grey color constant

--- src/constants.tsx
+++ src/constants.tsx
@@ -2,6 +2,7 @@ export const Colors = {
     blue: "#1F2E6E",
     red: "#FF3A3A",
     dark_grey: "#2D2D2F",
+    light_grey: "#8F8F8F",
     white: "#F5F5F5",
     white_accent: "#BDBDBD", // for pressed module threads
     yellow: "#EFB61E",
