5504dc5903437666af225fbefd11ee621c2ed642
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2023-01-07 00:58:19 +0700

    Add differing opacities to color constants

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,8 +1,13 @@
+/**
+ * Format: color_name_opacity
+ * If opacity is not defined, then it is 100% by default.
+ */
 export const Colors = {
     blue: "#1F2E6E",
     red: "#FF3A3A",
     dark_grey: "#2D2D2F",
     light_grey: "#8F8F8F",
+    light_grey_75: "#8F8F8F75",
     white: "#F5F5F5",
     white_accent: "#BDBDBD", // for pressed module threads
     yellow: "#EFB61E",
