817786740ad41937d73f0ef211744af280a12925
Jason Christopher <jason.christopher@u.nus.edu>
2023-01-14 15:12:00 +0800

    draft: new thread

--- src/slate.tsx
+++ src/slate.tsx
@@ -1,8 +1,30 @@
-import { BaseEditor, Descendant } from 'slate'
+import { BaseEditor } from 'slate'
+import { HistoryEditor } from 'slate-history'
 import { ReactEditor } from 'slate-react'
 
-type CustomElement = { type: 'paragraph'; children: CustomText[] }
-type CustomText = { text: string }
+export type CustomEditor = BaseEditor & ReactEditor & HistoryEditor
+
+export type ParagraphElement = {
+  type: 'paragraph'
+  children: CustomText[]
+}
+
+export type HeadingElement = {
+  type: 'heading'
+  level: number
+  children: CustomText[]
+}
+
+export type CodeElement = {
+  type: 'code'
+  children: CustomText[]
+}
+
+export type CustomElement = ParagraphElement | HeadingElement | CodeElement
+
+export type FormattedText = { text: string; bold?: true }
+
+export type CustomText = FormattedText
 
 declare module 'slate' {
   interface CustomTypes {
