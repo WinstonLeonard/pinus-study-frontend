368b732de4cb1fb5461d970f59b775045cd9b5f1
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2023-01-23 21:48:18 +0700

    Add functions to login modal

--- src/components/authentication_modal/LoginModal.tsx
+++ src/components/authentication_modal/LoginModal.tsx
@@ -1,7 +1,68 @@
-import { AuthButton, BlurredBackground, CloseIconDiv, ForgetPassword, ModalBackground, ModalDiv, ModalInput, ModalTitle, SwitchModalPrompt } from "./ModalComponents";
+import { AuthButton, BlurredBackground, CloseIconDiv, ErrorMessage, ForgetPassword, ModalBackground, ModalDiv, ModalInput, ModalTitle, SwitchModalPrompt } from "./ModalComponents";
+import { useEffect, useState } from "react";
+import { API_URL } from "../../constants";
 import CloseIcon from '@mui/icons-material/Close';
 
 const LoginModal = () => {
+
+    const [emailOrUsername, setEmailOrUsername] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [showError, setShowError] = useState<Boolean>(false);
+
+    const handleEmailOrUsernameChange = (event: any) => {
+        setEmailOrUsername(event.target.value);
+    }
+
+    const handlePasswordChange = (event: any) => {
+        setPassword(event.target.value);
+    }
+
+    const logIn = () => {
+        if (emailOrUsername.trim() === "" || password === "") {
+            setShowError(true);
+        } else {
+            fetch(API_URL + `/login`, {
+                method: "POST",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    username: emailOrUsername,
+                    email: emailOrUsername,
+                    password: password,
+                }),
+            }).then(response => response.json())
+            .then(data => {
+                if (data.status === "failure" || data.token === '') {
+                    setShowError(true);
+                } else {
+                    // #TODO: redux functions here
+                    sessionStorage.setItem("token", data.token);
+                }
+            })
+            .catch(error => console.log(error));
+        }
+    }
+
+    /**
+     * #TODO: Implement when backend is ready
+     */
+    const forgetPassword = () => {
+        // Implement code here
+    }
+
+    /**
+     * #TODO : Implement when router is implemented.
+     */
+    const navigateToSignUpModal = () => {
+        // Navigate here
+    }
+
+    useEffect(() => {
+        setShowError(false);
+    }, [emailOrUsername, password]);
+
     return (
         <BlurredBackground>
             <ModalBackground>
@@ -14,16 +75,21 @@ const LoginModal = () => {
                 <ModalDiv direction="column">
                     <ModalInput 
                         marginBottom="1em"
-                        placeholder="Email"/>
+                        placeholder="Email / Username"
+                        onChange={handleEmailOrUsernameChange}
+                        value={emailOrUsername}/>
                     <ModalInput 
                         placeholder = "Password"
-                        type="password"/>
+                        type="password"
+                        onChange={handlePasswordChange}
+                        value={password}/>
+                    { showError ? <ErrorMessage>Invalid login credentials!</ErrorMessage> : null }
                 </ModalDiv>
                 <ModalDiv justifyContent="center">
-                    <ForgetPassword onClick={()=>{}}>Forget Password?</ForgetPassword>
+                    <ForgetPassword onClick={forgetPassword}>Forget Password?</ForgetPassword>
                 </ModalDiv>
                 <ModalDiv>
-                    <AuthButton>Log In</AuthButton>
+                    <AuthButton onClick={logIn}>Log In</AuthButton>
                 </ModalDiv>
                 <ModalDiv/>
                 <ModalDiv/>
@@ -32,7 +98,7 @@ const LoginModal = () => {
                     <SwitchModalPrompt>Don't have an account?&nbsp;</SwitchModalPrompt>
                     <SwitchModalPrompt 
                         textDecoration="underline" 
-                        onClick={() => {}}
+                        onClick={navigateToSignUpModal}
                         cursor="pointer">Sign Up here.</SwitchModalPrompt>
                 </ModalDiv>
             </ModalBackground>
