1245f1c31598d65b808335911ab4f2640e694c27
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2023-03-19 21:17:31 +0800

    Comment out unused imports and functions

--- src/components/authentication_modal/CreateAccountModal.tsx
+++ src/components/authentication_modal/CreateAccountModal.tsx
@@ -3,7 +3,7 @@ import {
     BlurredBackground,
     CloseIconDiv,
     ErrorMessage,
-    ForgetPassword,
+    // ForgetPassword,
     ModalBackground,
     ModalDiv,
     ModalInput,
@@ -101,7 +101,8 @@ const CreateAccountModal = ({
                             setShowError(true);
                         } else {
                             // #TODO: redux functions here
-                            sessionStorage.setItem("token", data.token);
+                            localStorage.setItem("token", data.token);
+                            localStorage.setItem("userId", data.userid);
                             cancel();
                         }
                     })
@@ -110,12 +111,12 @@ const CreateAccountModal = ({
         }
     };
 
-    /**
-     * #TODO: Implement when backend is ready
-     */
-    const forgetPassword = () => {
-        // Implement code here
-    };
+    // /**
+    //  * #TODO: Implement when backend is ready
+    //  */
+    // const forgetPassword = () => {
+    //     // Implement code here
+    // };
 
     /**
      * Hides error message upon input to the Username or Password field.
