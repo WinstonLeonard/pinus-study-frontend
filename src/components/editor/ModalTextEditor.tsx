8dab4a58a7218eef973008fa231700e8949c89e9
Albert Ariel Widiaatmaja <w.albertariel@bytedance.com>
2023-04-15 16:34:04 +0800

    Search module bug fix

---
+++ src/components/editor/ModalTextEditor.tsx
@@ -0,0 +1,35 @@
+import { 
+    AuthButton, 
+    BlurredBackground, 
+    CloseIconDiv, 
+    ErrorMessage, 
+    ForgetPassword, 
+    ModalBackground, 
+    ModalDiv, 
+    ModalInput, 
+    ModalTitle, 
+    SwitchModalPrompt 
+} from "../authentication_modal/ModalComponents";
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from 'react-redux';
+import { login, selectToken } from '../../redux/features/users/userSlice';
+import { LOGIN_URL } from "../../constants";
+import CloseIcon from '@mui/icons-material/Close';
+import { getUserDetailsRequest } from "../../requests";
+import TextEditor from "./TextEditor"
+
+const TextEditorModal = ({cancel} : {cancel: () => void}) => {
+
+    return (
+        <BlurredBackground>
+            <ModalBackground>
+                <CloseIconDiv onClick={cancel}>
+                    <CloseIcon/>
+                </CloseIconDiv>
+                <TextEditor />
+            </ModalBackground>
+        </BlurredBackground>
+    )
+}
+
+export default TextEditorModal;
