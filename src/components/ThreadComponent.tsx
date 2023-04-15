016cf047dcbe4f6d6e802e753029d8507796cde1
Jason Christopher <jason.christopher@u.nus.edu>
2023-04-16 02:06:23 +0800

    Update deserializer to ThreadComponent

--- src/components/ThreadComponent.tsx
+++ src/components/ThreadComponent.tsx
@@ -16,6 +16,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { isLoggedIn } from '../utils';
 import { toggleSignup, toggleCreateAccount, toggleLogin } from '../redux/features/modal/modal';
 import CombinedAuthenticationPage from '../pages/CombinedAuthenticationPage';
+import { deserialize } from './editor/serializer';
 
 
 /** TODO: Add POST methods for likes (change functions in `<ThumbButton onClick={...}`) and upon submitting comment */ 
@@ -285,6 +286,21 @@ const ThreadComponent = ({threadId, type} : {threadId : number, type? : ThreadTy
     }
 
     /**
+     * Strips the HTML tags from the provided content string, and shortens
+     * the content to max. 150 characters by using the above `shortenLongPosts`
+     * function.
+     * 
+     * @param content The full content of the thread
+     * @returns The HTML-stripped, shortened content of the thread
+     */
+    const shortenRemoveHtml = (content : string) : string => {
+        const div = document.createElement("div");
+        div.innerHTML = content;
+        const text = div.textContent || div.innerText || "";
+        return shortenLongPosts(text);
+    }
+
+    /**
      * Parses the timestamp to produce the duration since the
      * thread was made.
      * 
@@ -329,7 +345,7 @@ const ThreadComponent = ({threadId, type} : {threadId : number, type? : ThreadTy
                     <br/>
                     <RegularText>Posted by @{thread.Username} in {thread.ModuleId}</RegularText>
                     <br/>
-                    <Content>{shortenLongPosts(thread.Content)}</Content>
+                    <Content>{shortenRemoveHtml(thread.Content)}</Content>
                     <br/>
                     <VerticalCenterAlignLayout>
                         <CommentOutlinedIcon sx={{fontSize: "1.375em"}}/>
@@ -357,7 +373,7 @@ const ThreadComponent = ({threadId, type} : {threadId : number, type? : ThreadTy
                 <br/>
                 <RegularText>Posted by <div onClick={directToUserPage}>@{thread.Username}</div></RegularText>
                 <br/>
-                <Content>{thread.Content}</Content>
+                <Content>{deserialize(thread.Content)}</Content>
                 <br/>
                 <VerticalCenterAlignLayout>
                     <ThumbButton onClick={handleLikeButton}>
