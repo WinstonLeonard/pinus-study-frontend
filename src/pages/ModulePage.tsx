fee631a4727acfa5a3eded04f39483418e8826a7
Berted <bertedberted@gmail.com>
2023-05-23 12:06:19 +0700

    Remove post threads button on guest mode; fixes CSS on post thread button

--- src/pages/ModulePage.tsx
+++ src/pages/ModulePage.tsx
@@ -9,91 +9,90 @@ import ThreadList from "../components/ThreadList";
 import { useSelector } from "react-redux";
 import { selectId, selectToken } from "../redux/features/users/userSlice";
 import TextEditor from "../components/editor/TextEditor";
-import { useState } from "react"
-import {Button} from "../components/editor/index"
+import { useState } from "react";
+
+import { isLoggedIn } from "../utils";
 
 const ModulePageWrapper = styled.div`
-    display: grid;
-    grid-template-columns: 8.5fr 1.5fr;
-    grid-column-gap: 2em;
-    padding: 2em;
-`
+  display: grid;
+  grid-template-columns: 8.5fr 1.5fr;
+  grid-column-gap: 2em;
+  padding: 2em;
+`;
 
 export const RightSide = styled.div`
-    display: flex;
-    flex-direction: column;
-`
+  display: flex;
+  flex-direction: column;
+`;
 
 const HeadingDiv = styled.div`
-    display: flex;
-    vertical-align: middle;
-    margin-bottom: 1em;
-    width: 100%;
-`
+  display: flex;
+  vertical-align: middle;
+  margin-bottom: 1em;
+  width: 100%;
+`;
 
 const Heading = styled.span`
-    width: 50%;
-    font-family: "Poppins", "sans-serif";
-    font-weight: 600;
-    font-size: 2em;
-    color: ${Colors.white};
-`
+  width: 50%;
+  font-family: "Poppins", "sans-serif";
+  font-weight: 600;
+  font-size: 2em;
+  color: ${Colors.white};
+`;
 
 const ButtonDiv = styled.div`
-    width: 50%;
-    display: flex;
-    justify-content: end;
-`
+  width: 50%;
+  display: flex;
+  justify-content: end;
+`;
 
 const ThreadListContainer = styled.div`
-    margin-left: 8px;
-`
+  margin-left: 8px;
+`;
 
 const ModulePage = () => {
-    const { mod } = useParams();
-    const userId = useSelector(selectId);
-    const token = useSelector(selectToken);
-    const [openTextEditor, setOpenTextEditor] = useState(false);
+  const { mod } = useParams();
+  const userId = useSelector(selectId);
+  const token = useSelector(selectToken);
+  const [openTextEditor, setOpenTextEditor] = useState(false);
 
-    const showTextEditor = () => {
-        setOpenTextEditor(true);
-    }
+  const showTextEditor = () => {
+    setOpenTextEditor(true);
+  };
 
-    const closeTextEditor = () => {
-        setOpenTextEditor(false);
-    }
+  const closeTextEditor = () => {
+    setOpenTextEditor(false);
+  };
 
-    return (
-        <div>
-            {openTextEditor ? <TextEditor closeTextEditor={closeTextEditor}/> : null}
-            <NavigationBar/>
-            <Background>
-                <ModulePageWrapper>
-                    <div>
-                        <HeadingDiv>
-                            <Heading>
-                                Discussion Forum
-                            </Heading>
-                            <ButtonDiv>
-                                <RedButton onClick={showTextEditor}>
-                                    + New Post
-                                </RedButton>
-                            </ButtonDiv>
-                        </HeadingDiv>
-                        <ThreadListContainer>
-                            <ThreadList selectedModule={mod? mod.toString() : ""}/>
-                        </ThreadListContainer>
-                        
-                    </div>
-                    <RightSide>
-                        <ModuleForum selectedModule={mod? mod.toString() : ""}/>
-                        <MyModules />
-                    </RightSide>
-
-                </ModulePageWrapper>
-            </Background>
-        </div>
-    )
-}
+  return (
+    <div>
+      {openTextEditor ? <TextEditor closeTextEditor={closeTextEditor} /> : null}
+      <NavigationBar />
+      <Background>
+        <ModulePageWrapper>
+          <div>
+            <HeadingDiv>
+              <Heading>Discussion Forum</Heading>
+              <ButtonDiv>
+                {isLoggedIn(token, userId) ? (
+                  <RedButton onClick={showTextEditor}>+ New Post</RedButton>
+                ) : (
+                  <></>
+                )}
+              </ButtonDiv>
+            </HeadingDiv>
+            <ThreadListContainer>
+              <ThreadList selectedModule={mod ? mod.toString() : ""} />
+            </ThreadListContainer>
+          </div>
+          <RightSide>
+            <ModuleForum selectedModule={mod ? mod.toString() : ""} />
+            <MyModules />
+          </RightSide>
+        </ModulePageWrapper>
+      </Background>
+    </div>
+  );
+};
 
 export default ModulePage;
