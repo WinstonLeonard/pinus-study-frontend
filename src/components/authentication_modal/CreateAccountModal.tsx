81a9ab80d5ba288c0591e28678359471b6c50c88
Elben85 <elbert.benedict@gmail.com>
2023-05-25 21:59:27 +0800

    Add Login Loading

--- src/components/authentication_modal/CreateAccountModal.tsx
+++ src/components/authentication_modal/CreateAccountModal.tsx
@@ -16,37 +16,8 @@ import { API_URL } from "../../constants";
 import CloseIcon from "@mui/icons-material/Close";
 import { useDispatch } from "react-redux";
 import { login } from "../../redux/features/users/userSlice";
+import { Loader } from "../Loader"
 import styled from "styled-components";
-
-const Loader = styled.span`
- {
-    width: 1.5em;
-    height: 1.5em;
-    border-radius: 50%;
-    display: inline-block;
-    position: relative;
-    background: linear-gradient(0deg, rgba(255, 61, 0, 0.2) 33%, #ff3d00 100%);
-    box-sizing: border-box;
-    animation: rotation 1s linear infinite;
-  }
-  &::after {
-    content: '';  
-    box-sizing: border-box;
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
-    width: 1.3em;
-    height: 1.3em;
-    border-radius: 50%;
-    background: #263238;
-  }
-  @keyframes rotation {
-    0% { transform: rotate(0deg) }
-    100% { transform: rotate(360deg)}
-  } 
-  }  
-`
 
 const CreateAccountModal = ({
     email,
